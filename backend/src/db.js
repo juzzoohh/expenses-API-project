f400f3ebc9224a5e640a0979768f3b21b2c43731
juzzoohh <juzzo0789@gmail.com>
2025-12-06 14:21:37 +0700

    deploy

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+require('dotenv').config();
+const { Pool } = require('pg');
+
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
+  ssl: process.env.PGHOST !== 'localhost' ? {
+    rejectUnauthorized: false
+  } : false
 });
 
 pool.query('SELECT NOW ()', (err, res) => {
